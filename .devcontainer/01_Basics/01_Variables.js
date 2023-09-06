766e31c84227cc024af3554201e714f2b1d9b4d5
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2023-09-06 07:41:43 +0000

    datatype done

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail="asharibkamal1@gmail.com"
+var accountPassword= "12345"
+accountCity="pakistan"
+accountState;
+
+//accountId=2 not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountCity])
+
+
+/*
+
+prefer not to be used Var 
+because of issue in block scope and functional scope
+*/
---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";//treat all js code as new version of javascript
+
+alert("assfg")// we are using node not browser
+
+console.log(3
+    
+    +3)//code readability should be high
+console.log("asharib");
+
+
+let name="asharib"
+let age= 28
+let ishuman=true
+
+//premetive datatype
+
+/*
+number =>2 to power of 53
+bigint 
+string => ""
+boolean => true or false
+null => standalone value
+undefined => 
+
+symbol => unique
+
+------Objects----
+
+
+
+*/
+console.log(typeof("asharib"));
+console.log(typeof(accountEmail));
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
