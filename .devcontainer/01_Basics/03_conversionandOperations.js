de62cd0398ef650d2774a02640afab1e784d3fb2
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2023-09-06 14:54:44 +0000

    conversion done

---
+++ .devcontainer/01_Basics/03_conversionandOperations.js
@@ -0,0 +1,31 @@
+let score= 33abc
+console.log(typeof(score));
+let valueInNumber= Number(score)
+
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber)
+/*
+
+"33"=> 33
+"33abc" => nan
+true = > 1 ; false => 0
+
+
+
+*/
+let isloggedin = 1
+ let isbooleanloggedin = Boolean(isloggedin)
+ console.log(typeof(isbooleanloggedin))
+
+
+ /*
+
+ 1 => true ; 0 => false
+ "" => false 
+ "asharib" =>true
+ 
+
+
+ */
