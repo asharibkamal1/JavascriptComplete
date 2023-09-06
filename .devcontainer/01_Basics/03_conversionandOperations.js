06c15bfd4e85167ebd35ed05e34bd238f84594f0
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2023-09-06 15:15:41 +0000

    operators done

--- .devcontainer/01_Basics/03_conversionandOperations.js
+++ .devcontainer/01_Basics/03_conversionandOperations.js
@@ -25,7 +25,44 @@ let isloggedin = 1
  1 => true ; 0 => false
  "" => false 
  "asharib" =>true
- 
+
 
 
  */
+
+
+ // *************** Operation   *********************///
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log("1"+ 2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log((3+4)*5%3);//right choce use paranthesis
+
+
+console.log(true);//true
+console.log(+true);//1
+console.log(+"");//0
+
+
+let gamecounter=100
+gamecounter++
+++gamecounter
+console.log(gamecounter);
+
+//reads prefix or post fix 
+
+
+
+
+
+
+
+
+
+
