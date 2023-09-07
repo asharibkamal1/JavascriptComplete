e32cd75f56ada83cf154eaba0cd20701886ec4ac
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2023-09-07 10:54:06 +0000

    maths done

---
+++ 01_basics/07_Maths_numbers.js
@@ -0,0 +1,28 @@
+const score= 100
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString())
+console.log(balance.toFixed(2));
+
+const othernumber = 23.892323
+console.log(othernumber.toPrecision(2));
+
+const hundreds= 10000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-IN'))
+console.log(hundreds.toLocaleString('en-PK'))
+
+
+//************ maths */4
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.random());
